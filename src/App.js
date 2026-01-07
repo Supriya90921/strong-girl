b4b71762796b1cdaf7a184689a66610c2954e04a
Supriya Gaikwad <supriyasanjug@gmail.com>
2026-01-07 15:06:58 +0530

    Initial commit - Strong Girl React app with hero section and period tracker

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import HeroSection from "./Components/Herosection";
 import Navbar from "./Components/Navbar";
 import PeriodTracker from "./Components/PeriodTracker";
 
+import Features from "./Components/Features";
+
 
 function App() {
   return (
     <>
       <Navbar />
+     
+    
       <HeroSection/>
+      
       <PeriodTracker/>
+
+        <Features />
     </>
   );
 }
