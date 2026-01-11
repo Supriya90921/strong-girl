b305f5444e1407da5a4ae04a05d646ecf2588190
Supriya Sanjay Gaikwad <138199157+Supriya90921@users.noreply.github.com>
2026-01-11 23:57:45 +0530

    Import DailyTracker component in App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import HeroSection from "./Components/Herosection";
 import Navbar from "./Components/Navbar";
 import PeriodTracker from "./Components/PeriodTracker";
-
+import Features from "./Components/DailyTracker";
 import Features from "./Components/Features";
 
 
